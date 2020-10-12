ee9a7703445cb052f8c10704d4834b3372e457e1
Manuel Garcia <manueldev250799@gmail.com>
2020-10-11 22:53:36 -0500

    Add jest and eslint

---
+++ src/__tests__/test.test.ts
@@ -0,0 +1,5 @@
+describe("Sample Test describe", () => {
+  it("Initial test", () => {
+    expect(2 + 2).toBe(4);
+  });
+});
--- src/controllers/rootController.ts
+++ src/controllers/rootController.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
-const rootController = (req: Request, res: Response) => {
+const rootController = (req: Request, res: Response): Response => {
   return res.send("Root Dir");
 };
 
