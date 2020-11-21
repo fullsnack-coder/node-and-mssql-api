341881e36a5f0c61e02c0d00ebe70271cff2ae57
ManuelDev <33798495+fullsnack-coder@users.noreply.github.com>
2020-11-21 17:35:45 -0500

    feat: add categories controller (#2)

--- src/routes/rootRouter.ts
+++ src/routes/rootRouter.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import rootController from '@controllers/rootController';
 import platesRoutes from './plates';
 import ingredientsRouter from './ingredients';
+import categoriesRouter from './categories';
 
 const rootRouter = Router();
 
@@ -9,6 +10,7 @@ function wrapRouter(router: Router) {
   router.get('/', rootController);
   router.use('/plates', platesRoutes);
   router.use('/ingredients', ingredientsRouter);
+  router.use('/categories', categoriesRouter);
 }
 
 wrapRouter(rootRouter);
